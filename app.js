d48cbe281c58d3cdcfe99c655664872c98310194
sayakanoa <89767987+sayakanoa@users.noreply.github.com>
2022-01-05 17:45:13 +0900

    app.js編集（while文）

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
-let i;
-let num = 0;
 
-for(i = 1; i < 11; i++){
-  num = num + i;
-}
+let num = 100;
+let count = 0;
 
-alert('1から10まで足し算した結果は' + num + 'です');
+ while (num !== 0) {
+   num = num - 2;
+   count = count + 1;
+ }
+
+//for (let i = 0; num !== 0; i++) {
+  //num = num - 2;
+  //count ++ ;
+  //if (num === 0) {
+    //break;
+  //}
+//}
+
+alert('100から２を引いて0になる回数は' + count + 'です');
