3a67e7de4413233e295dbd572119f6c1e7441469
sayakanoa <89767987+sayakanoa@users.noreply.github.com>
2022-01-07 22:31:15 +0900

    スコープ

--- sample.js
+++ sample.js
@@ -1,4 +1,9 @@
-var nickname = "hanako"
-console.log(nickname)
-nickname = "ichiro"
-console.log(nickname)
+var str = "webcamp" //グローバルスコープ
+
+function foo() {
+  console.log(str)
+  var y = "hello" //関数スコープ
+}
+
+foo()
+console.log(y)
