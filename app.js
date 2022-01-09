c993dcada1a6cbdc70254e911b6b2d9f49da11d0
sayakanoa <89767987+sayakanoa@users.noreply.github.com>
2022-01-09 16:34:33 +0900

    関数

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
+// メイン部分
+let alertString;
+alertString = addString("Web");
 
-let num = 100;
-let count = 0;
+// 作成した関数を呼び出す
+alert(alertString);
 
- while (num !== 0) {
-   num = num - 2;
-   count = count + 1;
- }
-
-//for (let i = 0; num !== 0; i++) {
-  //num = num - 2;
-  //count ++ ;
-  //if (num === 0) {
-    //break;
-  //}
-//}
-
-alert('100から２を引いて0になる回数は' + count + 'です');
+// 作成した関数
+function addString(strA){
+  let addStr = "Hello " + strA;
+  return addStr;
+}
